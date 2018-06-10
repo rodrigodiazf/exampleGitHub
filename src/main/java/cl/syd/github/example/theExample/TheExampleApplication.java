0373ea3e97363840544224cf77c0f4b1fa1ab751
Rodrigo Diaz <rodrigodiaz@Rodrigos-Air.iptv.microsoft.com>
2018-06-09 23:57:25 -0400

    Innitial commit

---
+++ src/main/java/cl/syd/github/example/theExample/TheExampleApplication.java
@@ -0,0 +1,12 @@
+package cl.syd.github.example.theExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/cl/syd/github/example/theExample/TheExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.syd.github.example.theExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
